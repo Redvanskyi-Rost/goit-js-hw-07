dd4fb23f694b4bdd75b4f65a0436c3935f30e0ff
Rost Redvanskyi <redvanskyirost@gmail.com>
2022-11-19 11:10:28 +0200

    add HW2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const imageContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryList(galleryItems);
 
-console.log(galleryItems);
+imageContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryList(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+// console.log(galleryItems);
